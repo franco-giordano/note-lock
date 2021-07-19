4895185e1a1ceb01872fded049cfc46a506c81c1
franco-giordano <francogior98@gmail.com>
2021-07-19 18:44:21 -0300

    better ui, lint code

--- src/App.svelte
+++ src/App.svelte
@@ -1,29 +1,45 @@
 <script lang="ts">
-	import Button from '@smui/button';
-	import Select, { Option } from '@smui/select';
-	import Textfield from '@smui/textfield';
+	import Button from "@smui/button";
+	import Select, { Option } from "@smui/select";
+	import Textfield from "@smui/textfield";
 	// import { Label } from '@smui/common';
-	import Snackbar, { Label } from '@smui/snackbar';
-	import Paper, { Title, Content } from '@smui/paper';
-	import { lockNote } from './functions/utils';
-	import IconButton from '@smui/icon-button';
-	import LayoutGrid, { Cell } from '@smui/layout-grid';
-	import Dialog, { Title as TitleDialog,  Content as ContentDialog, Actions } from '@smui/dialog';
+	import Snackbar, { Label } from "@smui/snackbar";
+	import Paper, { Title, Content } from "@smui/paper";
+	import { lockNote } from "./functions/utils";
+	import IconButton from "@smui/icon-button";
+	import LayoutGrid, { Cell } from "@smui/layout-grid";
+	import Dialog, {
+		Title as TitleDialog,
+		Content as ContentDialog,
+		Actions,
+	} from "@smui/dialog";
+	import Tooltip, { Wrapper } from "@smui/tooltip";
+	import Icon from "@smui/textfield/icon";
+	import HelperText from "@smui/textfield/helper-text/index";
 
 	let open;
 
 	let clipboardSnackbar;
 
-	let noteValue: string = 'write your note <br> here!';
-	let hintValue: string = '';
-	let passwordValue: string = '';
-	let result: string = '';
- 
-	let choicesBackground = ['beige', 'cadetblue', 'aquamarine', 'white', 'lightcyan', 'mistyrose'];
-	let selectedBackground = 'aquamarine';
+	let noteValue: string = "write your note <br> here!";
+	let hintValue: string = "";
+	let passwordValue: string = "";
+	let result: string = "";
+
+	let choicesBackground = [
+		"beige",
+		"cadetblue",
+		"aquamarine",
+		"white",
+		"lightcyan",
+		"mistyrose",
+	];
+	let selectedBackground = "aquamarine";
 
 	function encrypt() {
-		lockNote(noteValue, passwordValue, hintValue, selectedBackground).then(x => result = x);
+		lockNote(noteValue, passwordValue, hintValue, selectedBackground).then(
+			(x) => (result = x)
+		);
 		open = true;
 	}
 
@@ -37,23 +53,153 @@
 
 <svelte:head>
 	<title>Note Lock</title>
-	<link rel="stylesheet" href="https://unpkg.com/svelte-material-ui/bare.css" />
+	<link
+		rel="stylesheet"
+		href="https://unpkg.com/svelte-material-ui/bare.css"
+	/>
 	<!-- Material Icons -->
 	<link
-	rel="stylesheet"
-	href="https://fonts.googleapis.com/icon?family=Material+Icons"
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/icon?family=Material+Icons"
 	/>
 	<!-- Roboto -->
 	<link
-	rel="stylesheet"
-	href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
 	/>
 	<!-- Roboto Mono -->
 	<link
-	rel="stylesheet"
-	href="https://fonts.googleapis.com/css?family=Roboto+Mono"
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css?family=Roboto+Mono"
 	/>
 </svelte:head>
+
+<main>
+	<!-- svelte-ignore a11y-autofocus -->
+	<LayoutGrid>
+		<Cell spanDevices={{ desktop: 8, tablet: 12, phone: 12 }}>
+			<Paper
+				elevation={6}
+				class="paper"
+				style="background-color: {selectedBackground}; transition: background-color 100ms linear;"
+			>
+				<div
+					class="message"
+					autofocus
+					bind:innerHTML={noteValue}
+					contenteditable="true"
+				/>
+			</Paper>
+		</Cell>
+
+		<Cell spanDevices={{ desktop: 4, tablet: 12, phone: 12 }}>
+			<Paper elevation={6} class="paper">
+				<Title>Configure</Title>
+
+				<LayoutGrid>
+					<Cell span={12}>
+						<Textfield
+							bind:value={hintValue}
+							class="full-width"
+							label="Hint"
+						>
+							<Icon class="material-icons" slot="leadingIcon"
+								>lightbulb</Icon
+							>
+							<HelperText slot="helper"
+								>This hint will appear next to the password
+								prompt</HelperText
+							>
+						</Textfield>
+					</Cell>
+					<Cell span={12}
+						><Textfield
+							class="full-width"
+							bind:value={passwordValue}
+							required
+							label="Password"
+							input$type="password"
+						>
+							<Icon class="material-icons" slot="leadingIcon"
+								>vpn_key</Icon
+							>
+							<HelperText slot="helper"
+								>The required password to unlock the note</HelperText
+							></Textfield
+						>
+					</Cell>
+
+					<Cell span={12}>
+						<Select
+							variant="outlined"
+							bind:value={selectedBackground}
+							label="Background color"
+							class="full-width"
+						>
+							{#each choicesBackground as oneBg}
+								<Option value={oneBg}>{oneBg}</Option>
+							{/each}
+						</Select>
+					</Cell>
+
+					<Cell span={12}>
+						<Button
+							disabled={passwordValue === ""}
+							class="full-width"
+							variant="raised"
+							on:click={encrypt}>lock!</Button
+						>
+					</Cell>
+				</LayoutGrid>
+
+				<!-- <Textfield input$disabled="true" variant="filled" bind:value={result} label="Locked Note URL">
+				<IconButton class="material-icons" slot="trailingIcon" on:click={saveToClipboard}>
+					content_copy
+				</IconButton>
+			</Textfield> -->
+			</Paper>
+		</Cell>
+	</LayoutGrid>
+
+	<Snackbar bind:this={clipboardSnackbar}>
+		<Label>Copied to clipboard!</Label>
+	</Snackbar>
+</main>
+
+<Dialog
+	bind:open
+	aria-labelledby="simple-title"
+	aria-describedby="simple-content"
+>
+	<!-- Title cannot contain leading whitespace due to mdc-typography-baseline-top() -->
+	<TitleDialog id="simple-title">Note Lock created</TitleDialog>
+	<ContentDialog id="simple-content">
+		When using the link below, a prompt will appear with the following hint:
+		"{hintValue}"
+		<div>
+			<Textfield
+				input$disabled="true"
+				variant="filled"
+				bind:value={result}
+				label="Locked Note URL"
+				class="full-width"
+			>
+				<IconButton
+					class="material-icons"
+					slot="trailingIcon"
+					on:click={saveToClipboard}
+				>
+					content_copy
+				</IconButton>
+			</Textfield>
+		</div>
+	</ContentDialog>
+	<Actions>
+		<Button on:click={() => {}}>
+			<Label>Close</Label>
+		</Button>
+	</Actions>
+</Dialog>
 
 <style>
 	/* main {
@@ -67,6 +213,10 @@
 		/* width: 33vw; */
 		height: 80vh;
 		/* display: flex */
+	}
+
+	* :global(.full-width) {
+		width: 100%;
 	}
 
 	@media screen and (max-width: 840px) {
@@ -90,78 +240,8 @@
 		text-align: center;
 		/* margin: auto; */
 		/* display: flex; */
-		overflow: hidden;
+		overflow-x: hidden;
 		justify-content: center;
 		align-items: center;
 	}
 </style>
-
-<main>
-	<!-- svelte-ignore a11y-autofocus -->
-	<LayoutGrid>
-		
-	<Cell spanDevices={{ desktop: 8, tablet: 12, phone: 12 }}>
-	<Paper elevation={6} class="paper" style="background-color: {selectedBackground}; transition: background-color 100ms linear;">
-		<div class="message" autofocus bind:innerHTML={noteValue} contenteditable="true"></div>
-	</Paper>
-	</Cell>
-
-	<Cell spanDevices={{ desktop: 4, tablet: 12, phone: 12 }}>
-	<Paper elevation={6} class="paper">
-		<Title>Configure</Title>
-
-		<Content>
-			<Textfield bind:value={hintValue} label="hint"></Textfield>
-			<Textfield bind:value={passwordValue} label="password" input$type="password"></Textfield>
-			
-			<div>
-			<Label>Choose background color:</Label>
-			<Select variant="outlined" bind:value={selectedBackground} label="Background color">
-				{#each choicesBackground as oneBg}
-				  <Option value={oneBg}>{oneBg}</Option>
-				{/each}
-			</Select>
-			</div>
-			
-			<!-- <Textfield input$disabled="true" variant="filled" bind:value={result} label="Locked Note URL">
-				<IconButton class="material-icons" slot="trailingIcon" on:click={saveToClipboard}>
-					content_copy
-				</IconButton>
-			</Textfield> -->
-
-			<Button variant="raised" on:click={encrypt}>
-				lock!
-			</Button>
-		</Content>
-	</Paper>
-	</Cell>
-	</LayoutGrid>
-
-	<Snackbar bind:this={clipboardSnackbar}>
-		<Label>Copied to clipboard!</Label>
-	</Snackbar>
-</main>
-
-<Dialog
-  bind:open
-  aria-labelledby="simple-title"
-  aria-describedby="simple-content"
->
-  <!-- Title cannot contain leading whitespace due to mdc-typography-baseline-top() -->
-  <TitleDialog id="simple-title">Dialog Title</TitleDialog>
-  <ContentDialog id="simple-content">Super awesome dialog body text?
-	<Textfield input$disabled="true" variant="filled" bind:value={result} label="Locked Note URL">
-		<IconButton class="material-icons" slot="trailingIcon" on:click={saveToClipboard}>
-			content_copy
-		</IconButton>
-	</Textfield>
-  </ContentDialog>
-  <Actions>
-    <Button on:click={() => {}}>
-      <Label>No</Label>
-    </Button>
-    <Button on:click={() => {}}>
-      <Label>Yes</Label>
-    </Button>
-  </Actions>
-</Dialog>
